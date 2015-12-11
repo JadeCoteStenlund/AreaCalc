e804fa5ebfdb40cdbdb0fded3fe19b637d5f8459
wullyjones <kurtisgibb@outlook.com>
2015-12-11 13:09:14 -0500

    Added 3 shapes

---
+++ AreaCalculator/AreaCalculator/Octagon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator
+{
+    public class Octagon : Shape
+    {
+        private double side;
+
+        public Octagon()
+        {
+            side = 0;
+        }
+
+        public Octagon(double side)
+        {
+            this.side = side;
+        }
+
+        public void setSide(double side)
+        {
+            this.side = side;
+        }
+
+        public override double getArea()
+        {
+            return Math.Round(2 * (Math.Sqrt(2) + 1) * Math.Pow(side, 2), 2);
+        }
+    }
+}
---
+++ AreaCalculator/AreaCalculator/Pentagon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator
+{
+    public class Pentagon : Shape
+    {
+        private double side;
+
+        public Pentagon()
+        {
+            side = 0;
+        }
+
+        public Pentagon(double side)
+        {
+            this.side = side;
+        }
+
+        public void setSide(double side)
+        {
+            this.side = side;
+        }
+
+        public override double getArea()
+        {
+            return Math.Round(0.25 * Math.Sqrt(5 * (Math.Sqrt(5) * 2 + 5)) * Math.Pow(side, 2), 2);
+        }
+    }
+}
